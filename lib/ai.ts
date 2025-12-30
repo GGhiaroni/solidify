1ea8f5dd15a71a6ce603800fdf61e47fd331f6b7
GGhiaroni <gghiaronitiziano@gmail.com>
2025-12-30 19:41:48 -0300

    chamada na api do Gemini funcionando e já populando o Supabase com os steps da jornada de estudo

--- lib/ai.ts
+++ lib/ai.ts
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
 export async function generateRoadmapContent(title: string, area: string) {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
   const prompt = `
   Atue como um Tech Lead Sênior de uma Big Tech (Google/Amazon). Eu sou seu mentorado (nível Júnior/Pleno).

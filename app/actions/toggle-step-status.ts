d7621f2915a2b22fc83902502dd5d087725d6405
GGhiaroni <gghiaronitiziano@gmail.com>
2025-12-31 19:03:29 -0300

    criando a server action responsável por atualizar o check de um passo no roadmap

---
+++ app/actions/toggle-step-status.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+export async function toggleStepStatus(
+  stepId: string,
+  isCompleted: boolean,
+  roadmapId: string
+) {
+  try {
+    await prisma.roadmapStep.update({
+      where: { id: stepId },
+      data: { isCompleted },
+    });
+
+    //caso eu consiga atualizar, já vou revalidar o cachê,
+    // forçando o Next.js a recalcular a barra de progresso.
+    revalidatePath(`/minha-jornada/${roadmapId}`);
+
+    return { success: true };
+  } catch (error) {
+    console.error("Erro ao atualizar este passo:", error);
+    return { success: false, error: "Erro ao atualizar status no banco." };
+  }
+}

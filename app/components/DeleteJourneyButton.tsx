a64fe44ee710cc8401e1f88ce841d3cbbc051db8
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-01 17:34:46 -0300

    finalizando o componente DeleteJourneyButton

--- app/components/DeleteJourneyButton.tsx
+++ app/components/DeleteJourneyButton.tsx
@@ -1,8 +1,18 @@
 "use client";
 
-import { AlertDialog, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
-import { AlertDialogContent } from "@radix-ui/react-alert-dialog";
+import { Loader2 } from "lucide-react";
 import { useState, useTransition } from "react";
 import { toast } from "react-toastify";
 import { deleteJourney } from "../actions/delete-journey";
@@ -33,7 +43,41 @@ export default function DeleteJourneyButton({
         </Button>
       </AlertDialogTrigger>
 
-      <AlertDialogContent className="bg-primary border-soft/20 text-light"></AlertDialogContent>
+      <AlertDialogContent className="bg-primary border-soft/20 text-light">
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Tem certeza que deseja deletar essa jornada?
+          </AlertDialogTitle>
+          <AlertDialogDescription className="text-soft">
+            Essa ação não poderá ser desfeita. Isso excluirá permanentemente sua
+            jornada e todo o progresso dos passos associados.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel className="bg-transparent border-soft/20 text-light hover:bg-medium hover:text-white">
+            Cancelar
+          </AlertDialogCancel>
+
+          <AlertDialogAction
+            onClick={(e) => {
+              e.preventDefault();
+              handleDelete();
+            }}
+            disabled={isPending}
+            className="bg-red-500 text-white hover:bg-red-600 border-0"
+          >
+            {isPending ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Excluindo...
+              </>
+            ) : (
+              "Sim, excluir jornada."
+            )}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
     </AlertDialog>
   );
 }

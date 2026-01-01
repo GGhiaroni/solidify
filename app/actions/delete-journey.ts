a183815c2b0e5fa37787a743528eed7345aed438
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-01 12:17:04 -0300

    server action responsavel por deletar jornada do banco de dados terminada

--- app/actions/delete-journey.ts
+++ app/actions/delete-journey.ts
@@ -2,11 +2,17 @@
 
 import { prisma } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function deleteJourney(roadmapId: string) {
   try {
     await prisma.roadmap.delete({ where: { id: roadmapId } });
 
     revalidatePath("/minhas-jornadas");
-  } catch (error) {}
+  } catch (error) {
+    console.error("Erro ao deletar jornada.", error);
+    return { success: false, error: "Erro ao excluir" };
+  }
+
+  redirect("/minhas-jornadas");
 }

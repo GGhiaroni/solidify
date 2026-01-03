0909a0d80fe95648b453d1d27d37115bbe69ef62
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-03 18:09:42 -0300

    finalizando a server action responsável por salvar manualmente o tempo de estudo no banco de dados

--- app/actions/study-session-log.ts
+++ app/actions/study-session-log.ts
@@ -1,13 +1,34 @@
 "use server";
 
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
 interface StudySessionLogProps {
-    minutes: number,
-    date: Date
+  minutes: number;
+  date: Date;
 }
 
-export async function studySessionLog() {
-    
-  return (
+export async function studySessionLog({ minutes, date }: StudySessionLogProps) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: "dev@solidify.com" },
+    });
 
-  )
+    if (!user) return { success: false, error: "Usuário não encontrado" };
+
+    await prisma.studySession.create({
+      data: {
+        userId: user.id,
+        duration: minutes,
+        createdAt: date,
+      },
+    });
+
+    revalidatePath("/");
+
+    return { success: true };
+  } catch (error) {
+    console.error("Erro ao registrar sessão de estudos.", error);
+    return { success: false, error: "Erro ao registrar sessão de estudos." };
+  }
 }

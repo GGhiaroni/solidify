2c4f0f09c6cdd7b2c6572d8d83f3604070f64616
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-04 09:37:05 -0300

    avancando com o context do pomodoro

--- app/context/PomodoroContext.tsx
+++ app/context/PomodoroContext.tsx
@@ -1,7 +1,41 @@
 "use client";
 
+import { createContext, ReactNode, useState } from "react";
+
 type TimerMode = "focus" | "short" | "long";
 
-export default function PomodoroContext() {
-  return <div>PomodoroContext</div>;
+//interface, o que vai ficar disponível para todas as páginas da aplicação;
+interface PomodoroContextType {
+  mode: TimerMode;
+  time: number;
+  isActive: boolean;
+  setMode: (mode: TimerMode) => void;
+  toggleTimer: () => void; //ligar/desligar o cronômetro;
+  resetTimer: () => void; //resetar o cronômetro;
+}
+
+//criando o contexto, que incialmente está vazio;
+const PomodoroContext = createContext({} as PomodoroContextType);
+
+export function PomodoroProvider({ children }: { children: ReactNode }) {
+  const [mode, setMode] = useState<TimerMode>("focus");
+  const [isActive, setIsActive] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+
+  const [initialTime, setInitialTime] = useState(25 * 60);
+
+  return (
+    <PomodoroContext.Provider
+      value={{
+        mode,
+        time,
+        isActive,
+        setMode,
+        toggleTimer: () => setIsActive(!isActive),
+        resetTimer: () => setIsActive(false),
+      }}
+    >
+      {children}
+    </PomodoroContext.Provider>
+  );
 }

9ed060ebdabb6610212a6f97068af68eade67a30
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-08 14:56:20 -0300

    criando o contexto do LofiContext

--- app/context/LofiContext.tsx
+++ app/context/LofiContext.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Station } from "@/lib/lofi-stations";
+import { Station, stations } from "@/lib/lofi-stations";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 interface LofiContextType {
   isPlaying: boolean;
@@ -10,3 +11,39 @@ interface LofiContextType {
   setVolume: (value: number) => void;
   changeStation: (stationId: string) => void;
 }
+
+const LofiContext = createContext({} as LofiContextType);
+
+export function LofiProvider({ children }: { children: ReactNode }) {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(0.5);
+  const [currentStation, setCurrentStation] = useState<Station>(stations[0]);
+
+  const togglePlay = () => setIsPlaying((prev) => !prev);
+
+  const changeStation = (stationId: string) => {
+    const newStation = stations.find((s) => s.id === stationId);
+
+    if (newStation) {
+      setCurrentStation(newStation);
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <LofiContext.Provider
+      value={{
+        isPlaying,
+        volume,
+        currentStation,
+        togglePlay,
+        setVolume,
+        changeStation,
+      }}
+    >
+      {children}
+    </LofiContext.Provider>
+  );
+}
+
+export const useLofi = () => useContext(LofiContext);

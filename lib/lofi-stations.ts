e2bcb58c41b84e7f59a9582f9b8bbd9ce5612300
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-08 21:31:18 -0300

    acertando as stations

--- lib/lofi-stations.ts
+++ lib/lofi-stations.ts
@@ -7,22 +7,10 @@ export interface Station {
 
 export const stations: Station[] = [
   {
-    id: "lofi-girl",
-    name: "Lofi Girl Radio",
-    streamUrl: "https://play.streamafrica.net/lofigirl",
-    color: "bg-orange-500",
-  },
-  {
     id: "chillhop",
     name: "Chillhop Raccoon",
     streamUrl: "https://stream.zeno.fm/f3wvbbqmdg8uv",
     color: "bg-blue-500",
-  },
-  {
-    id: "flux-fm",
-    name: "FluxFM Focus",
-    streamUrl: "https://fluxfm.streamabc.net/flx-focus?sabc=6254",
-    color: "bg-green-500",
   },
   {
     id: "smooth-chill",

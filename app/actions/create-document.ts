608550dec7e66414cbf60fb7d0b6ebaa3b5f2c8f
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-11 11:36:34 -0300

    criando a server actions responsável por criar documento

---
+++ app/actions/create-document.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export async function createDocument(parentDocumentId?: string) {
+  try {
+    const clerkUser = await currentUser();
+
+    if (!clerkUser || !clerkUser.emailAddresses[0]) {
+      return { success: false, error: "Usuário não autenticado." };
+    }
+
+    const userEmail = clerkUser.emailAddresses[0].emailAddress;
+
+    const dbUser = await prisma.user.findUnique({
+      where: {
+        email: userEmail,
+      },
+    });
+
+    if (!dbUser) {
+      return { success: false, error: "Usuário não encontrado no banco." };
+    }
+
+    const document = await prisma.document.create({
+      data: {
+        title: "Sem título",
+        userId: dbUser.id,
+        parentDocumentId: parentDocumentId || null,
+        isArchived: false,
+      },
+    });
+
+    revalidatePath("/notas");
+    return { success: true, documentId: document.id };
+  } catch (error) {
+    console.log("Erro ao criar nota:", error);
+    return { success: false, error: "Erro interno" };
+  }
+}

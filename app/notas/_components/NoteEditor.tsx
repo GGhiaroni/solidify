45bfb4788dd7c1aeab1cdae5f0e62033766b35c2
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-12 14:51:48 -0300

    adicionando o focus no bloco de notas para melhorar a ux

--- app/notas/_components/NoteEditor.tsx
+++ app/notas/_components/NoteEditor.tsx
@@ -5,6 +5,7 @@ import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import { useCreateBlockNote } from "@blocknote/react";
+import { useEffect } from "react";
 
 interface EditorProps {
   onChange?: (value: string) => void;
@@ -22,6 +23,14 @@ export default function NoteEditor({
       ? (JSON.parse(initialContent) as PartialBlock[])
       : undefined,
   });
+
+  useEffect(() => {
+    if (editor && editable) {
+      setTimeout(() => {
+        editor.focus();
+      }, 100);
+    }
+  }, [editor, editable]);
 
   return (
     <div className="editor-wrapper w-full max-w-full">

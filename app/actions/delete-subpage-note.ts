faa8e4af890e3ae14b405502ffc947002e4b25b8
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-17 13:02:29 -0300

    criando a server action responsável por deletar uma nota

---
+++ app/actions/delete-subpage-note.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export default async function deleteSubPageNote(subpageId: string) {
+  const clerkUser = await currentUser();
+
+  if (!clerkUser || !clerkUser.emailAddresses[0]) {
+    return { success: false, error: "Usuário não autenticado." };
+  }
+
+  const email = clerkUser.emailAddresses[0].emailAddress;
+
+  try {
+    const result = await prisma.document.deleteMany({
+      where: {
+        id: subpageId,
+        user: {
+          email: email,
+        },
+      },
+    });
+
+    if (result.count === 0) {
+      return {
+        success: false,
+        error: "Nota não encontrada ou usuário sem permissão.",
+      };
+    }
+    revalidatePath("/cadernos");
+    return { success: true };
+  } catch (error) {
+    console.error("Erro ao deletar nota.", error);
+    return { success: false, error: "Erro ao deletar nota." };
+  }
+}
